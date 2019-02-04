d9e3760c1dc44c10f2ad93e4ffa134d77fbf77bd
David Nies <david.nies@adition.com>
2019-02-04 14:19:42 +0100

    Update Clojure to 1.7.0
    Clojure 1.5.1 does not contain the function `when-some`. This causes
    `lein repl` to fail because nREPL could not be loaded.
    
    To reproduce this error, add this to you `~.lein/profiles.clj`:
    
    [org.clojure/tools.nrepl "0.2.13"]

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/pulseio/parquet-cascalog/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [cascalog "2.0.0"]
                  [com.twitter/parquet-column "1.2.4"]
                  [com.twitter/parquet-hadoop "1.2.4"]
